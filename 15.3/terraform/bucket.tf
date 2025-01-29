0ae788690707a74adece6aaed6ad0144108cc60c
valery-dubinin <valery.dubinin@gmail.com>
2025-01-29 15:30:41 +0300

    Подготовлен терраформ код для 1 задания

---
+++ 15.3/terraform/bucket.tf
@@ -0,0 +1,53 @@
+// Create SA
+resource "yandex_iam_service_account" "sa" {
+  folder_id = var.folder_id
+  name      = "tf-test-sa"
+}
+
+// Grant permissions
+resource "yandex_resourcemanager_folder_iam_member" "sa-editor" {
+  folder_id = var.folder_id
+  role      = "editor"
+  member    = "serviceAccount:${yandex_iam_service_account.sa.id}"
+}
+
+// Create Static Access Keys
+resource "yandex_iam_service_account_static_access_key" "sa-static-key" {
+  service_account_id = yandex_iam_service_account.sa.id
+  description        = "static access key for object storage"
+}
+
+// Create encryprion key
+resource "yandex_kms_symmetric_key" "encryptkey" {
+ name              = "encryptkey"
+ default_algorithm = "AES_256"
+ rotation_period   = "8760h"
+}
+
+
+// Use keys to create bucket
+resource "yandex_storage_bucket" "test" {
+  access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
+  secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
+  bucket     = "dubininvk-test-bucket"
+  acl        = "public-read"
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = yandex_kms_symmetric_key.encryptkey.id
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+}
+
+//Uploading image to bucket
+resource "yandex_storage_object" "test-picture" {
+  access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
+  secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key  
+  bucket = yandex_storage_bucket.test.id
+  key    = "shedevr.jpg"
+  source = "./shedevr.jpg"
+  acl = "public-read"
+  depends_on = [yandex_storage_bucket.test]
+}
